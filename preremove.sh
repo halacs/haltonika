11913b639df6ebc7e1f3001223b7ba78147072ea
Gábor Nyíri <halacs@users.noreply.github.com>
2026-01-31 16:33:18 +0100

    remove some leftover when purge deb package

--- preremove.sh
+++ preremove.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 set -e
+
+# Always stop and disable the service
 systemctl stop haltonika.service
 systemctl disable haltonika.service
+
+case "$1" in
+    remove)
+        ;;
+
+    purge)
+        # Logical "Purge": Clean up everything
+        echo "Purging all data and configurations..."
+        #rm -rf /etc/haltonika/haltonika.met
+        rm -rf /etc/haltonika
+        ;;
+
+    upgrade|failed-upgrade|abort-install|abort-upgrade|disappear)
+        ;;
+
+    *)
+        exit 0
+        ;;
+esac
